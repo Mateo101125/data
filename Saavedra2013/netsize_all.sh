64c9f0d407017c6883d26520569324827bfdd83b
Mateo101125 <104949461+Mateo101125@users.noreply.github.com>
2022-05-30 02:24:46 -0500

    comentarios en los codigos
    Mateo coloco comentarios

--- Saavedra2013/netsize_all.sh
+++ Saavedra2013/netsize_all.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
-for (( c=1; c<=59; c++))
-do 
-echo n$c.txt >> netsize_all.txt
+#!/bin/bash #llama al bash 
+for (( c=1; c<=59; c++)) #me hace el bucle de 1 a 59 que son los documentos
+do  #me dice la accion que va hace
+# aqui utilice el codigo que utilice a en netzise pero cambie el $1 por 
+# $c para que  me tome la accion que señale en el for  de 1 a 59
+echo n$c.txt >> netsize_all.txt 
 echo "FILAS" >> netsize_all.txt
 cut -d " " -f 1 n$c.txt | tail -n +1 | wc -l  >> netsize_all.txt
 echo "COLUMNAS" >> netsize_all.txt
 head -n +1 n$c.txt | grep -o " "|wc -l  >> netsize_all.txt
 done
-
+# el done indica el cierre de la accion 
--- Saavedra2013/netzise.sh
+++ Saavedra2013/netzise.sh
@@ -1,7 +1,15 @@
-ECHO $1 > netsize.txt
+#para que me coloque el numero de documento
+echo $1 <> netsize.txt 
+# para que me escriba fila
 echo "FILAS" >> netsize.txt
+#codigo para que me cuente la fila, cut para que me corte solo la fila, tail 
+#me va  contar desde abajo hacia arriba y  wc se para que me de el valor 
+#numerico en especifico el -l para que me de la fila
 cut -d " " -f 1  $1 | tail -n +1 | wc -l  >> netsize.txt
+#para que me escriba columna
 echo "COLUMNAS" >> netsize.txt
+# head para que comience desde la primera, grep para que me cuente los espacios
+# horizontalmente y wc -l para que me de el valor numerico
 head -n +1 $1| grep -o " "|wc -l  >> netsize.txt
 
 
